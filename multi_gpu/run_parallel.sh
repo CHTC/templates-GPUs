92484ac3cf8630a6d09ef80c4dda7e386066f41c
Anthony Gitter <gitter@biostat.wisc.edu>
2022-09-15 16:22:18 -0500

    Update conda install to match PyTorch example

--- multi_gpu/run_parallel.sh
+++ multi_gpu/run_parallel.sh
@@ -3,17 +3,19 @@ set -e
 
 # installation steps for Miniconda
 export HOME=$PWD
-export PATH
-sh Miniconda3-latest-Linux-x86_64.sh -b -p $PWD/miniconda3
-export PATH=$PWD/miniconda3/bin:$PATH
+sh Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda3
+rm Miniconda3-latest-Linux-x86_64.sh
+export PATH=$HOME/miniconda3/bin:$PATH
+source $HOME/miniconda3/etc/profile.d/conda.sh
+hash -r
+conda config --set always_yes yes --set changeps1 no
 
 # install environment
-conda env create -f env.yml -n env.yml
-source activate base 
-conda activate env.yml
+conda env create -f env.yml -n multigpu
+conda activate multigpu
 
 #wandb login <your api key>
 
 # Edit the argument after -n to reflect how many GPUs were requested in the submit file
-python3 model_parallel.py -n 3
+python3 model_parallel.py -n 1
 
