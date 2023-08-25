4cd6c773e8f378a1e7570a456b3b6dfff958a36b
Jason Lo <JasonLo@users.noreply.github.com>
2023-08-25 09:48:32 -0500

    Update llm/run.sh
    Co-authored-by: Anthony Gitter <agitter@users.noreply.github.com>

--- llm/run.sh
+++ llm/run.sh
@@ -9,4 +9,4 @@ export HF_MODULES_CACHE=$_CONDOR_SCRATCH_DIR/modules
 export HF_METRICS_CACHE=$_CONDOR_SCRATCH_DIR/metrics
 
 # You can remove the --use_wandb flag if tracking is not needed.
-python3 train.py demo_run --use_wandb
+python3 train.py $1 --use_wandb
