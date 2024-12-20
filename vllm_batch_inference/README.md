9c237fbacc6a8cdb2b0191e924bfa1dac1cd4bf9
Jason Lo <lcmjlo@gmail.com>
2024-12-20 12:51:06 -0600

    simplify and clean

--- vllm_batch_inference/README.md
+++ vllm_batch_inference/README.md
@@ -5,28 +5,28 @@ Below is an example workflow demonstrating how to set up, package, submit, and r
 - Generating large volumes of synthetic data using open-source LLMs.
 - Conducting large-scale, structured data extraction from text.
 - Embedding large volumes of text.
-- Running any LLM-driven tasks cost-effectively and at massive scale, without relying on expensive commercial alternatives.
+- Running any LLM-driven tasks cost-effectively at massive scale, without relying on expensive commercial alternatives.
 
 ## Prerequisites
 
-- You already know the basics on CHTC, HTCondor, Docker universe job, and staging.
-- You are familiar with OpenAI python API (we are using the same syntax to call open source models).
+- You already know the basics on CHTC, HTCondor, Docker universe job.
 
 ## Step-by-step guide
+
+1. Go to [huggingface](https://huggingface.co/settings/tokens) and get a token. You need it for downloading open source models.
+1. `ssh` to a submit node
+1. Make a `.env` file based on this [example](.env.example)
+1. `condor submit job.sub`
 
 ## FAQ
 
 1. How to find supported open source model?
 
-    Go to [Huggingface's model](https://huggingface.co/models), pick one, you can check if it supports vllm by clicking `use this model` ![hugging face vllm](img/hf-vllm.png). Please read the model instruction as some model requires approval or signing an user agreement.
+    Go to [Huggingface's model](https://huggingface.co/models), pick one, you can check if it supports vllm by clicking `use this model` ![hugging face vllm](img/hf-vllm.png). Please read the model instruction as some model requires approval or signing an user agreement on their website.
 
 1. Why vllm?
 
     Currently, vllm has the higher throughput that I know of for batch offline inference.
-
-1. Does this example allow multiple parallel jobs?
-
-    Since I want to keep things relatively simple, I used a `JSONL` file to store the results in this example. However, it is not really design for too many concurrent write. You probably should use a proper database like `PostgreSQL` if you are running into write file issue.
 
 ## About the author
 
---
+++ vllm_batch_inference/batch_inference.py
@@ -0,0 +1,64 @@
+import json
+from pathlib import Path
+
+import vllm
+
+
+def load_data(file: Path) -> list[dict[str, str]]:
+    """Load data from a jsonl file. Assuming at least `id` and `input` fields are present.
+
+    Example input format: (docs: https://jsonlines.org/)
+    {"id": "x0001", input": "What is the capital of France?"}
+    {"id": "x0002", input": "What is the capital of Germany?"}
+    """
+
+    with open(file, mode="r") as f:
+        return [json.loads(line) for line in f]
+
+
+def format_prompt(user_message: str, system_message: str | None = None) -> str:
+    """This function formats the user message into the correct format for the `Phi-3.5-mini-instruct` model.
+
+    docs: https://huggingface.co/microsoft/Phi-3.5-mini-instruct#input-formats
+    """
+
+    if system_message is None:
+        system_message = (
+            "You are a helpful assistant. Provide clear and concise responses."
+        )
+    return (
+        "<|system|>\n"
+        f"{system_message}<|end|>\n"
+        "<|user|>\n"
+        f"{user_message}<|end|>\n"
+        "<|assistant|>\n"
+    )
+
+
+def inference(data: list[dict], input_field: str = "input") -> list[dict]:
+    """Perform a batch of inference on Phi-3.5-mini-instruct via vllm offline mode."""
+
+    inputs = [item[input_field] for item in data]
+    formatted_inputs = [format_prompt(user_message=x) for x in inputs]
+    llm = vllm.LLM(model="microsoft/Phi-3.5-mini-instruct")
+    outputs = llm.generate(prompts=formatted_inputs)
+    text_outputs = [raw_output.outputs[0].text.strip() for raw_output in outputs]
+    return [{"output": output, **item} for item, output in zip(data, text_outputs)]
+
+
+def save_data(data: list[dict], file: Path) -> None:
+    """Save data to a jsonl file in append mode.
+
+    Example output format:
+    {"id": "x0001", "input": "What is the capital of France?", "output": "Paris."}
+    {"id": "x0002", "input": "What is the capital of Germany?", "output": "Berlin."}
+    """
+    with open(file, mode="a") as f:
+        for item in data:
+            f.write(json.dumps(item) + "\n")
+
+
+# Load data, perform inference, and save data
+data = load_data(Path("inputs.jsonl"))
+outputs = inference(data)
+save_data(outputs, Path("outputs.jsonl"))
